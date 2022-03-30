04530037f20c9d5679e671e498a7b323748d74f8
erdawyne <erdawynee1@gmail.com>
2022-03-30 15:27:09 +0700

    Merunning employee

--- CobaDataGridView/Form1.cs
+++ CobaDataGridView/Form1.cs
@@ -16,5 +16,12 @@ namespace CobaDataGridView
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'adventureWorksDataSet2.Employee' table. You can move, or remove it, as needed.
+            this.employeeTableAdapter.Fill(this.adventureWorksDataSet2.Employee);
+
+        }
     }
 }
